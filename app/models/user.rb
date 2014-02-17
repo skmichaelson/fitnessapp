6a500f39791dbc21216bb8cd579fe2630f672e3e
Sarah Michaelson <Sarah.Michaelson@gmail.com>
2014-02-17 11:34:33 -0500

    Complete validations on User attributes, including session_token and password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,28 @@ class User < ActiveRecord::Base
                   :gender
   attr_reader :password
 
+  before_validation :ensure_session_token!
+
+  validates :username, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password_digest, presence: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :gender, presence: true, inclusion: { in: %w(M F) }
+  validates :current_wt, :goal_wt, :height, :activity_level, :birthday, presence: true
+
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  private
+
+  def ensure_session_token!
+    self.session_token ||= User.generate_session_token
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,73 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "missing vital properties" do
+    let(:invalid_user) {User.new}
+
+    it "validates presence of vital info" do
+      expect(invalid_user).to have(1).error_on(:username)
+      expect(invalid_user).to have(1).error_on(:password_digest)
+      expect(invalid_user).to have(1).error_on(:email)
+      expect(invalid_user).to have(1).error_on(:current_wt)
+      expect(invalid_user).to have(1).error_on(:goal_wt)
+      expect(invalid_user).to have(1).error_on(:height)
+      expect(invalid_user).to have(1).error_on(:activity_level)
+      expect(invalid_user).to have(1).error_on(:birthday)
+      expect(invalid_user).to have(2).error_on(:gender)
+    end
+
+    it "checks for uniqueness on username and email" do
+      user1 = User.create!({username: "user",
+        password: "password",
+        email: "user1@example.com",
+        current_wt: 150,
+        gender: "F",
+        goal_wt: 130,
+        height: 62,
+        activity_level: 1,
+        birthday: 19850107
+        })
+
+      user2 = User.new({username: "user",
+        password: "password",
+        email: "user2@example.com",
+        current_wt: 150,
+        gender: "F",
+        goal_wt: 130,
+        height: 62,
+        activity_level: 1,
+        birthday: 19850107
+        })
+
+      user3 = User.new({username: "user",
+        password: "password",
+        email: "user1@example.com",
+        current_wt: 150,
+        gender: "F",
+        goal_wt: 130,
+        height: 62,
+        activity_level: 1,
+        birthday: 19850107
+        })
+
+        expect(user2).not_to be_valid
+        expect(user3).not_to be_valid
+    end
+
+    it "assigns a session token upon creation of a user" do
+      user1 = User.create!({username: "user",
+        password: "password",
+        email: "user1@example.com",
+        current_wt: 150,
+        gender: "F",
+        goal_wt: 130,
+        height: 62,
+        activity_level: 1,
+        birthday: 19850107
+        })
+
+      expect(user1.session_token).not_to be_nil
+    end
+
+  end
 end
