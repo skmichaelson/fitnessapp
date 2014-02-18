87d15b7a1c65de622dc30f7dee8c8ea0e3c00e6c
Sarah Michaelson <Sarah.Michaelson@gmail.com>
2014-02-18 14:35:31 -0500

    Update current_user method to avoid hitting DB multiple times

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :require_logged_in
 
   def current_user
-    User.find_by_session_token(session[:session_token])
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by_session_token(session[:session_token])
   end
 
   def require_logged_in
