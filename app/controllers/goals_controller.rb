7b19a48490dce670021a86b33a3cb8a14122bc93
Sarah Michaelson <Sarah.Michaelson@gmail.com>
2014-02-18 17:43:36 -0500

    Form now works for creation of a goal

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -4,5 +4,21 @@ class GoalsController < ApplicationController
   def new
     @goal = current_user.build_goal
   end
+
+  def create
+    @goal = current_user.build_goal
+    @goal.update_attributes(params[:goals])
+
+    if @goal.save
+      redirect_to goal_url(@goal)
+    else
+      flash.now[:errors] = @goal.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @goal = Goal.find(params[:id])
+  end
 end
 
