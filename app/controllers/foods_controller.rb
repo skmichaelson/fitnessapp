85ee0b5334b1f21c9102529e7ea063e2d22cbfdb
Sarah Michaelson <Sarah.Michaelson@gmail.com>
2014-02-19 12:14:49 -0500

    Create working food search

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,6 +4,7 @@ class FoodsController < ApplicationController
 
   def search
     food_name = params[:food][:name]
-    @foods = Food.where("food.name LIKE %?%", food_name)
+    @foods = Food.where("name LIKE ?", "%#{food_name}%")
+    render :index
   end
 end
