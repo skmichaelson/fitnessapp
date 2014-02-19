3cd77ea5179884f31e821d1c16346582294cdb0d
Sarah Michaelson <Sarah.Michaelson@gmail.com>
2014-02-19 15:28:56 -0500

    Refactor food search feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,4 +35,12 @@ class ApplicationController < ActionController::Base
 
     adjusted_attr
   end
+
+  def serving_size
+    params[:servings] ? params[:servings][:size].to_f : 100
+  end
+
+  def num_servings
+    params[:servings] ? params[:servings][:number].to_f : 1
+  end
 end
--- app/helpers/foods_helper.rb
+++ app/helpers/foods_helper.rb
@@ -1,2 +1,23 @@
 module FoodsHelper
+  def calculate_by_serving(food, serving_size, num_servings)
+    nutrition_attributes = food.attributes.reject do |attr|
+      %w(id name usda_id common_weight_1 common_weight_2 common_serving_1 common_serving_2 created_at updated_at).include?(attr)
+    end
+
+    adjusted_attr = {}
+
+    nutrition_attributes.each do |attr, val|
+      adjusted_attr[attr] = (val * serving_size * num_servings / 100)
+    end
+
+    adjusted_attr
+  end
+
+  def serving_size
+    params[:servings] ? params[:servings][:size].to_f : 100
+  end
+
+  def num_servings
+    params[:servings] ? params[:servings][:number].to_f : 1
+  end
 end
