7142b64d7895311c8ca874138ee9e04329262da2
App Academy Student <student@appacademy.io>
2014-02-27 11:05:00 -0500

    Fix styling for friend and user index

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,9 +1,9 @@
 class FriendRequestsController < ApplicationController
-  
+
   def create
     new_friend = User.find(params[:friend_id])
     friend_request = new_friend.friend_requests.new(friend_id: current_user.id)
-    
+
     if friend_request.save
       flash[:notices] = ["Request sent!"]
       redirect_to users_url
@@ -11,13 +11,13 @@ class FriendRequestsController < ApplicationController
       flash.now[:errors] = friend_request.errors.full_messages
       render 'users/index'
     end
-    
+
   end
-  
+
   def destroy
     request = FriendRequest.find(params[:id])
     request.destroy
-    flash[:notices] = ["Request denied"]
-    redirect_to friends_user_url(current_user)
+    flash[:notices] = ["Friend request cancelled"]
+    redirect_to users_url
   end
 end
