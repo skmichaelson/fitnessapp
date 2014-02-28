f79f6736c7e2d48f3be7d55ba67357db7b823c14
Sarah Michaelson <Sarah.Michaelson@gmail.com>
2014-02-28 10:55:07 -0500

    Allow signed in users to add new foods to DB

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,8 @@
 class FoodsController < ApplicationController
   include FoodsHelper
 
+  before_filter :require_logged_in, only: [:new, :create]
+
   def index
     @entry = params[:daily_entry_id] ? DailyEntry.find(params[:daily_entry_id]) : nil
   end
@@ -9,6 +11,7 @@ class FoodsController < ApplicationController
     food_name = params[:food][:name]
     if food_name.strip.empty? || food_name.length < 2
       @foods = []
+      flash.now[:errors] = ["Please enter a valid search string!"]
     end
     @entry = params[:daily_entry_id] ? DailyEntry.find(params[:daily_entry_id]) : nil
     @foods ||= Food.where("name LIKE ?", "%#{food_name}%").page(params[:page])
@@ -24,4 +27,26 @@ class FoodsController < ApplicationController
   def new
     @food = Food.new
   end
+
+  def create
+    @food = Food.new(params[:food])
+
+    if params[:serving][:size].empty? || params[:serving][:size] == 0
+      flash.now[:errors] = ["Please enter a serving size!"]
+      render new
+      return
+    end
+
+    if @food.valid?
+      multiplier = 100 / (params[:serving][:size].to_f * params[:serving][:unit].to_f)
+      @food.calories *= multiplier
+      @food.save
+      flash[:notices] = "#{@food.name} successfully added to the database!"
+      redirect_to food_url(@food)
+    else
+      flash.now[:errors] = @food.errors.full_messages
+      render new
+    end
+  end
+
 end
