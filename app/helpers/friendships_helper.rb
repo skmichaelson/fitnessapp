f883d14f2759d79f8f5cb78472aae383a361ab26
Sarah Michaelson <sarah.michaelson@gmail.com>
2014-03-07 17:04:56 -0500

    New friendships automatically generate messages

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,23 @@
 module FriendshipsHelper
+  def new_friend_message_body(friend)
+    message = <<-MESSAGE_BODY
+    Greetings! \n\n
+    
+    We just wanted to let you know that #{friend.username} has accepted
+    your friend request. You should check out their profile or send them a message! \n\n
+    
+    Find more friends on this site by visiting the user index page. 
+    Remember, you're more likely to achieve your goals if you stay accountable to each other! \n\n
+      
+    Best of luck, \n\n
+    
+    The AppleCinnamon Team
+    MESSAGE_BODY
+    
+    return message.html_safe
+  end
+  
+  def new_friend_subject_line
+    "You have a new friend!"
+  end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,6 @@
 class Friendship < ActiveRecord::Base
+  include FriendshipsHelper
+  
   attr_accessible :user_id, :friend_id, :status
 
   belongs_to :friend, class_name: "User", foreign_key: :friend_id
@@ -7,12 +9,20 @@ class Friendship < ActiveRecord::Base
   validates :user_id, uniqueness: { scope: :friend_id, message: "Already friends with this user!" }
   
   after_save :remove_pending_request
+  after_save :send_greeting
   
   private
   
   def remove_pending_request
     req = User.find(self.user_id).friend_requests.where("friend_id = ?", self.friend_id).first
     req ? req.destroy : true
+  end
+  
+  def send_greeting
+    Message.create(recipient_id: self.user_id, 
+                   sender_id: self.friend_id, 
+                   subject: new_friend_subject_line, 
+                   body: new_friend_message_body(User.find(friend_id)))
   end
 
 end
