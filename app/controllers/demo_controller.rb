f1995ea74cc84c2d4d58fab4ae7240122b17cf5b
Sarah Michaelson <sarah.michaelson@gmail.com>
2014-03-07 10:23:07 -0500

    Add styled demo notices to pages

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,6 +3,7 @@ class DemoController < ApplicationController
   
   def new_user
     @user = create_demo_user
+    flash.now[:demo] = ["<h1>Welcome, and thanks for trying my app!</h1>".html_safe, "Please choose a password to continue"]
     render 'users/new'
   end
 end
