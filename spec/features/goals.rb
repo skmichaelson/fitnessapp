80937b399d8870821b65ea8e5409f559becd40f0
Sarah Michaelson <Sarah.Michaelson@gmail.com>
2014-03-12 02:53:52 -0400

    Update Goal specs

---
+++ spec/features/goals.rb
@@ -0,0 +1,10 @@
+describe "User goals" do
+  context "calculation of values" do
+    it "should determine BMR to the nearest integer" do
+    end
+
+    it "should determine calorie goal" do
+    end
+  end
+
+end
--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 describe Goal do
-
-  context "setting defaults" do
-
-    it "has the correct default values" do
-    end
-
-  end
-
-  context "autogenerating values" do
-
-    it "should calculate BMR to the nearest integer" do
-    end
-
-    it "should calculate calorie goal" do
-    end
-  end
+  it { should allow_mass_assignment_of(:user_id) }
+  it { should allow_mass_assignment_of(:bmr) }
+  it { should allow_mass_assignment_of(:calorie_goal) }
+  it { should allow_mass_assignment_of(:fat_ratio) }
+  it { should allow_mass_assignment_of(:carb_ratio) }
+  it { should allow_mass_assignment_of(:protein_ratio) }
+  it { should allow_mass_assignment_of(:sodium) }
+  it { should allow_mass_assignment_of(:potassium) }
+  it { should allow_mass_assignment_of(:cholesterol) }
+  it { should allow_mass_assignment_of(:calcium) }
+  it { should allow_mass_assignment_of(:vitamin_a) }
+  it { should allow_mass_assignment_of(:vitamin_c) }
+  it { should allow_mass_assignment_of(:iron) }
+  it { should allow_mass_assignment_of(:workouts_per_week) }
+  it { should allow_mass_assignment_of(:minutes_per_workout) }
+  it { should allow_mass_assignment_of(:weight_loss_rate) }
 
   describe "associations" do
     it { should belong_to(:user) }
