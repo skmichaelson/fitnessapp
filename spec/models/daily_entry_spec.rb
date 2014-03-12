4a422da8054b4af59e6abc69e6896cac4ee1bd9b
Sarah Michaelson <Sarah.Michaelson@gmail.com>
2014-03-12 02:09:31 -0400

    Add factory method to generate DailyEntries

--- spec/models/daily_entry_spec.rb
+++ spec/models/daily_entry_spec.rb
@@ -5,5 +5,23 @@ describe DailyEntry do
   describe "associations" do
     it { should belong_to(:diary) }
     it { should have_many(:meal_entries) }
+    it { should have_many(:foods) }
+    it { should have_many(:feed_items) }
+    it { should have_many(:report_items) }
+  end
+
+  describe "default values" do
+    it "should set defaults" do
+      new_entry = DailyEntry.new
+      expect(new_entry).to have(0).errors_on(:calories_consumed)
+      expect(new_entry).to have(0).errors_on(:calories_burned)
+      expect(new_entry).to have(0).errors_on(:fat_intake)
+      expect(new_entry).to have(0).errors_on(:carbohydrate_intake)
+      expect(new_entry).to have(0).errors_on(:protein_intake)
+      expect(new_entry).to have(0).errors_on(:calcium_intake)
+      expect(new_entry).to have(0).errors_on(:iron_intake)
+      expect(new_entry).to have(0).errors_on(:vitamin_a_intake)
+      expect(new_entry).to have(0).errors_on(:vitamin_c_intake)
+    end
   end
 end
